5b5c3f8b177c57358f5589e2b43a68651d7a9bfd
krobison10 <kylerrobison10@outlook.com>
2024-02-17 05:19:10 -0800

    create modal refactor

--- components/CreateModal.jsx
+++ components/CreateModal.jsx
@@ -1,98 +1,27 @@
 'use client';
-import React from 'react';
-import {Box, Button, Tab, Tabs, Typography} from '@mui/material';
-import {useState, memo} from 'react';
+import React, {useState} from 'react';
+import {Button} from '@mui/material';
+import {memo} from 'react';
 import PropTypes from 'prop-types';
 
-import Modal from '@/components/common/Modal';
-import CreateApplicationForm from '@/components/applications/CreateApplicationForm';
-import CreateInterviewForm from '@/components/interviews/CreateInterviewForm';
+import CreateModalContent from '@/components/CreateModalContent';
 
-function TabPanel(props) {
-  const {children, value, index, ...other} = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box className='p-3'>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired,
-};
 
 const CreateModal = memo(function CreateModal({tabIndex}) {
   const [open, setOpen] = useState(false);
-
-  const openModal = () => {
+  function openModal() {
     setOpen(true);
-  };
-
-  const closeModal = () => {
+  }
+  function closeModal() {
     setOpen(false);
-  };
-
-  function CreateModalTabs() {
-    const [tab, setTab] = useState(tabIndex || 0);
-
-    const handleChange = (event, newTab) => {
-      setTab(newTab);
-    };
-
-    return (
-      <Box className='w-[640px] mt-2'>
-        <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
-          <Tabs value={tab} onChange={handleChange} aria-label="create-options">
-            <Tab label="Application" />
-            <Tab label="Interview" />
-            <Tab label="Goal"/>
-          </Tabs>
-        </Box>
-        <TabPanel value={tab} index={0}>
-          <CreateApplicationForm closeModal={closeModal}/>
-        </TabPanel>
-        <TabPanel value={tab} index={1}>
-          <CreateInterviewForm closeModal={closeModal}/>
-        </TabPanel>
-        <TabPanel value={tab} index={2}>
-          <div className='h-[800px]'>
-
-          </div>
-        </TabPanel>
-      </Box>
-    );
   }
-
-  function CreateModalContent() {
-    return (
-      <CreateModalTabs/>
-    );
-  }
-
   return (
     <>
       <Button variant='contained' className='mx-2' color='success' onClick={() => openModal()}>Create</Button>
-      <Modal
+      <CreateModalContent
+        tabIndex={tabIndex}
         isOpen={open}
-        title='Create'
-        content={<CreateModalContent/>}
-        closeModal={closeModal}
-        maxWidth='md'
-        fullWidth={false}
-        closeOffFocus={false}/>
+        closeModal={closeModal} />
     </>
   );
 });
